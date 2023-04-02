4eb87451c828083534df287087109e9759ffb5e8
Dapper-d <oladapoomoworare@gmail.com>
2023-04-02 15:33:37 +0100

    .

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import "./App.css";
 import Image from "next/image";
-import images from "./component/img";
+import images from "../../public/img";
 import {
   Button,
   Divider,
